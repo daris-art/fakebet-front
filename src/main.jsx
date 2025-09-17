da20bdeda814b0877d7c9394f591a14312a63c3d
daris-art <baksicdaris1@gmail.com>
2025-09-17 12:43:18 +0200

    correction bug toast de confirmation pari

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,32 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Toaster
+      position="bottom-right"
+      toastOptions={{
+        style: {
+          background: "#1f1f1f",
+          color: "#fff",
+          border: "1px solid #333",
+        },
+        success: {
+          iconTheme: {
+            primary: "#22c55e", // vert
+            secondary: "#1f1f1f",
+          },
+        },
+        error: {
+          iconTheme: {
+            primary: "#ef4444", // rouge
+            secondary: "#1f1f1f",
+          },
+        },
+      }}
+    />
   </React.StrictMode>
 );
